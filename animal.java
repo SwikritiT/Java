995ac03f40a2cbace5c03f87428b2951f1dc2e86
Swikriti <swikriti808@gmail.com>
2020-04-18 14:42:23 +0545

    Animal.java

---
+++ animal.java
@@ -0,0 +1,15 @@
+class Animal
+{
+  public static void main(String[] args)
+ {
+   System.out.println("hello");
+  }
+}
+ class Dog
+{
+  public static void main(String[] args)
+  {
+    System.out.println("hi this is another class of same program");
+  }
+}
+
