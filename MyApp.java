111aaccd119f650eb387c570ee9ad99260ed586d
Swikriti <swikriti808@gmail.com>
2020-04-19 17:31:30 +0545

    java

---
+++ MyApp.java
@@ -0,0 +1,8 @@
+import pkg1.Sample;
+class MyApp{
+	public static void main(String[] args)
+	{
+	MyApp ma=new MyApp();
+	ma.result();
+	}
+}
---
+++ switcheg.java
@@ -0,0 +1,15 @@
+class Switcheg
+{
+ public static void main(String[] args)
+ {
+   char ch='a';
+ switch (ch)
+{
+ case 'a' : System.out.println("a");
+ case 'b' : System.out.println("b");
+ case 'c' : System.out.println("c"); break;
+ case 'd' : System.out.println("d"); break;
+ default : System.out.println("sorry");
+     }
+   }
+}
